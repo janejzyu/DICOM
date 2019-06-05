c608b93f578ea116f42c07296d74c877e69141be
janejzyu <yujiangzhen@hotmail.com>
2019-06-04 23:37:09 -0700

    finished model with epoch

--- src/model.py
+++ src/model.py
@@ -2,34 +2,42 @@ import numpy as np
 import config
 
 class Model():
-    """Create a epoch
-    """
-    def __init__(self, dataset, batch_size = 8, seed = 64):
-        self.dataset = dataset
-        self.batch_size = batch_size
-        self.epoch_generator = self._generate_train_epoch(batch_size)
-        self.seed = seed
-        np.random.seed(seed)
-        
-    def _generate_index(self, batch_size):
-        n = self.dataset.n_dat
-        rand_index = np.random.permutation(n)
-        start = 0
-        end = start + batch_size
-        while start < n:
-            yield rand_index[start:end]
-            start = end
-            end = start + batch_size
-    
-    def _generate_train_epoch(self, batch_size):
-        for ind in self._generate_index(batch_size = batch_size):
-            x, y = self.dataset.get_data(ind)
-            yield x, y, ind
-    
-    def next_batch(self):
-        try:
-            return next(self.epoch_generator)
-        except StopIteration:
-            print('finished a epoch')
-            self.epoch_generator = self._generate_train_epoch(self.batch_size)
-            return next(self.epoch_generator)
+	"""Create a model with input data for trainining pipleline
+	"""
+	def __init__(self, dataset, epochs = 1, batch_size = 8, seed = 64):
+		self.dataset = dataset
+		self.batch_size = batch_size
+		self.epochs = epochs
+		self.cur_epoch = 1
+		self.epoch_generator = self._generate_train_epoch(batch_size)
+		self.seed = seed
+		np.random.seed(seed)
+		
+	def _generate_index(self, batch_size):
+		"""Generator of random index of batch_size
+		"""
+		n = self.dataset.n_dat
+		rand_index = np.random.permutation(n)
+		start = 0
+		end = start + batch_size
+		while start < n:
+			yield rand_index[start:end]
+			start = end
+			end = start + batch_size
+	
+	def _generate_train_epoch(self, batch_size):
+		"""Generator of training data of batch_size
+		"""
+		for ind in self._generate_index(batch_size = batch_size):
+			x, y = self.dataset.get_data(ind)
+			yield x, y, ind
+	
+	def next_batch(self):
+		try:
+			return next(self.epoch_generator)
+		except StopIteration:
+			print('finished epoch {}'.format(self.cur_epoch))
+			self.cur_epoch += 1
+			if self.cur_epoch <= self.epochs:
+				self.epoch_generator = self._generate_train_epoch(self.batch_size)
+				return next(self.epoch_generator)
--- src/training.py
+++ src/training.py
@@ -9,29 +9,34 @@ from dataset import Dataset
 
 pair = pd.read_csv(config.PAIR_PATH)
 
-# def get_arg(arg):
-# 	if arg in sys.argv:
-# 		return int(sys.argv[sys.argv.index(arg)+1])
+param_list = {'batch_size': 8, 'epochs': 5, 'seed': 63}
+
+
+def get_arg(param):
+	arg = '-' + param
+	if arg in sys.argv:
+		param_list[param] = int(sys.argv[sys.argv.index(arg)+1])
 
 
 
-batch_size = 8
-if '-batch_size' in sys.argv:
-	batch_size = int(sys.argv[sys.argv.index('-batch_size')+1])
+for param in param_list:
+	get_arg(param)
 
-epochs = 5
-if '-epochs' in sys.argv:
-	epochs = int(sys.argv[sys.argv.index('-epochs')+1])
 
 if __name__ == "__main__":
 
 	dataset = Dataset(x_path = pair[config.DICOM_FILE_TYPE], 
 					  y_path = pair[config.MASK_FILE_TYPE])
-	model = Model(dataset, batch_size = batch_size)
-	for i in range(epochs*batch_size):
+	
+	model = Model(dataset, 
+		          epochs = param_list['epochs'],
+		          batch_size = param_list['batch_size'],
+		          seed = param_list['seed'])
+	
+	while True:
 		try:
 			x, y, j = model.next_batch()
-			print(x.shape)
+			print(j)
 		except TypeError:
-			print('end')
+			print('end of training loop')
 			break
